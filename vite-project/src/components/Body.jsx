51ca5df86ef3b19624f2817927b621cbbd72bbc6
guiflopes2008 <guilhermefreitaslopes146@gmail.com>
2025-05-26 22:20:19 -0300

    feath: introdução do React

---
+++ vite-project/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body'>
+            <h2 className='titulo2'>dinos são legais</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas hic asperiores a vitae ut qui iure aut commodi, doloribus quasi labore iste quos, beatae ex sapiente excepturi debitis laborum delectus.</p>
+        </div>
+    )
+}
+
+export default Body
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='Header'>
+            <h1>Dinossauros🦕☄️🦖</h1>
+        </div>
+    )
+}
+
+export default Header
